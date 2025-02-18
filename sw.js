fea1374b1b9fadf6273b45bda2fb50a34fbd9147
kmarena <ricardo.camarena@proton.me>
2025-02-18 00:27:52 -0600

    segundo commit: correccion manifest

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'mis-horas-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png',
+  './fontawesome/css/all.min.css',
+  './registro-manual.html',
+  './registros.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then(response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          });
+      })
+  );
+});
